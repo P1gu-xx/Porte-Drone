2a488cd37f90a0b4178f0b8757f1f9a7b5b5bf9c
PeclatJ <PeclatJ@WEMFA4508.student.fr.ch>
2014-04-04 15:05:44 +0200

    ajout de la fonction ton pere

--- Serveur/src/ch/emf/portedrone/application/Application.java
+++ Serveur/src/ch/emf/portedrone/application/Application.java
@@ -16,8 +16,12 @@ public class Application {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
     public static void tamere() {
+        System.out.println("ce");
+    }
+
+    public static void tonpere() {
         System.out.println("ce");
     }
 }
